4d0f4cd517004b86524cd05e86be7926c79dd511
VAC2UM <nikita.golowanev@gmail.com>
2024-04-05 17:49:57 +0300

    Выполнен 1-й пункт

--- app/src/main/java/com/itproger/sixthprak/MainActivity.java
+++ app/src/main/java/com/itproger/sixthprak/MainActivity.java
@@ -3,7 +3,6 @@ package com.itproger.sixthprak;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
