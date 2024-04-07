41dc06f1178e5625abcc3f6dd244a17ad526a4ee
VAC2UM <nikita.golowanev@gmail.com>
2024-04-07 14:03:46 +0300

    Выполнение 2-го пункта

--- app/src/main/java/com/itproger/sixthprak/MainActivity.java
+++ app/src/main/java/com/itproger/sixthprak/MainActivity.java
@@ -2,40 +2,56 @@ package com.itproger.sixthprak;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout drawer;
+    public ActionBarDrawerToggle toogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
-            @Override
-            public void onNavigationItemReselected(@NonNull MenuItem item) {
-                if (item.getItemId() == R.id.home){
-                    Intent intent = new Intent(MainActivity.this, Home.class);
-                    startActivity(intent);
-//                    Toast.makeText(MainActivity.this, "Домой", Toast.LENGTH_LONG).show();
-                }
-                else if (item.getItemId() == R.id.settings) {
-                    Intent intent = new Intent(MainActivity.this, Settings.class);
-                    startActivity(intent);
-//                    Toast.makeText(MainActivity.this, "Настройки", Toast.LENGTH_LONG).show();
-                }
-                else if (item.getItemId() == R.id.notification) {
-                    Intent intent = new Intent(MainActivity.this, Notifications.class);
-                    startActivity(intent);
-//                    Toast.makeText(MainActivity.this, "Уведомления", Toast.LENGTH_LONG).show();
-                }
+        drawer = findViewById(R.id.my_drawer_layout);
+        toogle = new ActionBarDrawerToggle(MainActivity.this, drawer, R.string.drawer_open, R.string.drawer_close);
+
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            int id = menuItem.getItemId();
+
+            if(id == R.id.nav_home){
+                Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                MainActivity.this.startActivity(intent);
             }
+            return true;
         });
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toogle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
---
+++ app/src/main/java/com/itproger/sixthprak/SecondaryActivity.java
@@ -0,0 +1,52 @@
+package com.itproger.sixthprak;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Главная");
+
+            BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+            bottomNavigationView.setOnNavigationItemReselectedListener(item -> {
+                if (item.getItemId() == R.id.home) {
+                    actionBar.setTitle("Главная");
+                    Intent intent = new Intent(SecondaryActivity.this, MainActivity.class);
+                    startActivity(intent);
+//                    Toast.makeText(MainActivity.this, "Домой", Toast.LENGTH_LONG).show();
+                } else if (item.getItemId() == R.id.settings) {
+                    actionBar.setTitle("Настройки");
+                    Intent intent = new Intent(SecondaryActivity.this, Settings.class);
+                    startActivity(intent);
+//                    Toast.makeText(SecondaryActivity.this, "Настройки", Toast.LENGTH_LONG).show();
+                } else if (item.getItemId() == R.id.notification) {
+                    actionBar.setTitle("Уведомления");
+//                    Intent intent = new Intent(MainActivity.this, Notifications.class);
+//                    startActivity(intent);
+//                    Toast.makeText(MainActivity.this, "Уведомления", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu ){
+        getMenuInflater().inflate(R.menu.bottom_nav_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
